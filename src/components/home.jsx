5f7ead7c0e8211c5d61e606f4fb8f353af1683b3
ryzke <dazryanjake7@gmail.com>
2025-02-25 19:25:08 +0800

    home update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 function Home({ homeId }) {
   return (
     <>
-      <div className='w-full h-screen bg-amber-200 flex' id={homeId}>
-        <div className='w-[60%] h-full bg-amber-800 flex items-center'>
-          <div className='w-full h-1/3 bg-amber-400'>
-
+      <div className='w-full h-screen flex text-white' id={homeId}>
+        <div className='w-[50%] h-full flex items-center'>
+          <div className='w-full h-1/3 flex items-center flex-col'>
+            <p className='w-[60%] text-2xl'>Hello,</p>
+            <h1 className='w-[60%] text-5xl font-bold mb-2'>I'm Ryan Jake Daz</h1>
+            <p className='w-[60%] text-2xl'>a <span className='font-bold'>Frond-End Developer</span> in the Philippines</p>
           </div>
         </div>
-        <div className='w-[40%] h-full bg-blue-400'></div>
+        <div className='w-[50%] h-full'></div>
       </div>
     </>
   );
